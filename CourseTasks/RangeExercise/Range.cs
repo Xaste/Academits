a8408871015951a102a21c8b72b99f7f0bd8d328
xaste <xaste@mail.ru>
2018-07-15 19:21:52 +0700

    Получение интервала-пересечения двух интервалов

---
+++ CourseTasks/RangeExercise/Range.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeExercise
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public Range()
+        {
+
+        }
+
+        public double GetLength()
+        {
+            return From - To;
+        }
+
+        public bool IsInside(double a)
+        {
+            return a >= From && a <= To;
+        }
+
+        public static Range GetCrossRange(Range a, Range b)
+        {
+            if (a.To < b.From || a.From > b.To)
+            {
+                return null;
+            }
+
+            Range answer = new Range
+            {
+                From = a.From > b.From ? a.From : b.From,
+                To = a.To < b.To ? a.To : b.To
+            };
+
+            return answer;
+        }
+    }
+}
---
+++ CourseTasks/RangeExercise/RangeExercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeExercise
+{
+    class RangeExercise
+    {
+        static void Main(string[] args)
+        {
+            Range a = new Range(3, 8);
+            Range b = new Range(7, 8);
+
+            Range c = Range.GetCrossRange(a, b);
+            Console.WriteLine($"FROM = {c.From}    To = {c.To}");
+
+            Console.ReadLine();
+        }
+    }
+}
