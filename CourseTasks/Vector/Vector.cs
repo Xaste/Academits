041d5422d12610b0878022efa5e4918445dca031
xaste <xaste@mail.ru>
2018-07-19 12:32:51 +0700

    Начал делать вектор

---
+++ CourseTasks/Vector/Vector.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Vector
+    {
+        //public int VectorSize { get; set; }
+        private double[] vectorComponents;
+
+        public Vector(int n)
+        {
+            this.vectorComponents = new double[n];
+        }
+
+        public Vector(Vector vect)
+        {
+            //this.VectorSize = vect.GetSize();
+            this.vectorComponents = new double[vect.GetSize()];
+
+            for (int i = 0; i < vect.GetSize(); i++)
+            {
+                this.vectorComponents[i] = vect.vectorComponents[i];
+            }
+        }
+
+        public Vector(double[] array)
+        {
+            this.vectorComponents = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                this.vectorComponents[i] = array[i];
+            }
+        }
+
+        public Vector(int n, double[] array)
+        {
+            //this.VectorSize = n;
+
+            this.vectorComponents = new double[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                if (i < array.Length)
+                {
+                    this.vectorComponents[i] = array[i];
+
+                }
+                else
+                {
+                    this.vectorComponents[i] = 0;
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return vectorComponents.Length;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this.GetSize() - 1; i++)
+            {
+                sb.Append($"{this.vectorComponents[i]}, ");
+            }
+            sb.Append($"{this.vectorComponents[this.GetSize() - 1]}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CourseTasks/Vector/VectorExercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class VectorExercise
+    {
+        static void Main(string[] args)
+        {
+            Vector vect1 = new Vector(10);
+
+            Vector vect2 = new Vector(vect1);
+
+            double[] array = new double[] { 17, 22, 33, 14, 5 };
+            Vector vect3 = new Vector(array);
+
+            Vector vect4 = new Vector(10, array);
+
+            Console.WriteLine(vect4.ToString());
+            Console.WriteLine();
+        }
+    }
+}
