f7cfb14aae0b12458a9ae092dcce028a966bb1a1
xaste <xaste@mail.ru>
2018-07-23 15:52:58 +0700

    Matrix создал проект, добавил первый конструктор

---
+++ CourseTasks/MatrixExercise/Matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorExercise;
+
+namespace MatrixExercise
+{
+    class Matrix
+    {
+        public Vector[] Rows { get; set; }
+
+        public Matrix(int n, int m)
+        {
+            Rows = new Vector[n];
+            for (int i = 0; i < n; i++)
+            {
+                Rows[i] = new Vector(m);
+            }
+        }
+    }
+}
