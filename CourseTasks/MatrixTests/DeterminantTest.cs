31d3ce6497253a6a8454e44f62bc214eb1caa143
xaste <xaste@mail.ru>
2018-07-30 14:26:36 +0700

    Matrix fixes

--- CourseTasks/MatrixTests/DeterminantTest.cs
+++ CourseTasks/MatrixTests/DeterminantTest.cs
@@ -14,18 +14,14 @@ namespace MatrixTests
             var matrix1 = new Matrix(array1);
 
             var determinantAnswer1 = -24;
-
             var determinantResult1 = matrix1.GetDeterminant();
 
             Assert.AreEqual(determinantAnswer1, determinantResult1);
-
-
 
             var array2 = new double[,] { { 2, -1, 4 }, { 1, 0.7, -5 }, { 0, 3, 0 } };
             var matrix2 = new Matrix(array2);
 
             var determinantAnswer2 = 42;
-
             var determinantResult2 = matrix2.GetDeterminant();
 
             Assert.AreEqual(determinantAnswer2, determinantResult2);
