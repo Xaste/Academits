1c78bd44a7c733548b0fa26fe1f1a7d087fe42cf
xaste <xaste@mail.ru>
2018-07-31 09:48:44 +0700

    MyList

---
+++ CourseTasks/ListExercise/ListItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise
+{
+    class ListItem<T>
+    {
+        private T data;
+        private ListItem<T> next;
+
+        public ListItem(T data)
+        {
+            this.data = data;
+        }
+
+        public ListItem(T data, ListItem<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public T GetData()
+        {
+            return data;
+        }
+
+        public void SetData(T data)
+        {
+            this.data = data;
+        }
+
+        public ListItem<T> GetNext()
+        {
+            return next;
+        }
+
+        public void SetNext(ListItem<T> next)
+        {
+            this.next = next;
+        }
+    }
+}
---
+++ CourseTasks/ListExercise/MyList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise
+{
+    class MyList<T>
+    {
+        private ListItem<T> head;
+
+        public MyList(T data)
+        {
+            head = new ListItem<T>(data);
+        }
+
+        //public SetNext
+    }
+}
