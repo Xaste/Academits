6f611a1b114ac1996e00777c123a7ac55a22169b
xaste <xaste@mail.ru>
2018-08-16 14:51:44 +0700

    MyHashTable

--- CourseTasks/HashTableExercise/HashTableExercise.cs
+++ CourseTasks/HashTableExercise/HashTableExercise.cs
@@ -10,6 +10,15 @@ namespace HashTableExercise
     {
         static void Main(string[] args)
         {
+            var table1 = new MyHashTable<int>(new int[] { 22, 13, 55, 77, 8000 });
+
+            table1.Contains(223);
+            table1.Contains(8000);
+            table1.Remove(8000);
+
+            table1.Add(8000);
+            table1.Add(8000);
+
         }
     }
 }
--- CourseTasks/HashTableExercise/MyHashTable.cs
+++ CourseTasks/HashTableExercise/MyHashTable.cs
@@ -10,13 +10,37 @@ namespace HashTableExercise
     class MyHashTable<T> : ICollection<T>
     {
         private List<T>[] items = new List<T>[100];
-        
+
         public int Count { get; }
         public bool IsReadOnly { get; }
 
+        public MyHashTable(params T[] data)
+        {
+            foreach (var e in data)
+            {
+                Add(e);
+            }
+        }
+
+        public MyHashTable(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Размерность массива не может быть меньше 0");
+            }
+
+            items = new List<T>[capacity];
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            foreach (var t in items)
+            {
+                foreach (var VARIABLE in t)
+                {
+                    yield return VARIABLE;
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -32,17 +56,41 @@ namespace HashTableExercise
             }
 
             var index = Math.Abs(item.GetHashCode() % items.Length);
+
+            if (ReferenceEquals(items[index], null))
+            {
+                items[index] = new List<T>();
+            }
+
             items[index].Add(item);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < items.Length; i++)
+            {
+                items[i].Clear();
+            }
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var index = Math.Abs(item.GetHashCode() % items.Length);
+
+            if (ReferenceEquals(items[index], null) || items[index].Count == 0)
+            {
+                return false;
+            }
+
+            foreach (var e in items[index])
+            {
+                if (Equals(item, e))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -52,7 +100,15 @@ namespace HashTableExercise
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            if (!Contains(item))
+            {
+                return false;
+            }
+
+            var index = Math.Abs(item.GetHashCode() % items.Length);
+            items[index].Remove(item);
+
+            return true;
         }
 
     }
