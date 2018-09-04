49b86d576bfc2cbf006e396dfdbe571e18229590
xaste <xaste@mail.ru>
2018-09-04 11:48:44 +0700

    Serializing

---
+++ CourseTasks/SerializingExercise/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializingExercise
+{
+    [Serializable]
+    class Rectangle
+    {
+        private double height;
+        private double width;
+
+        [NonSerialized]
+        private double area;
+
+        public Rectangle (double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+
+            area = width * height;
+        }
+
+        [OnDeserialized]
+        private void GetArea(StreamingContext context)
+        {
+            area = height * width;
+        }
+    }
+}
---
+++ CourseTasks/SerializingExercise/SerializingExercise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializingExercise
+{
+    class SerializingExercise
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(5, 2);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (var output = new FileStream("out.bin", FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(output, rectangle);
+            }
+
+            Rectangle result;
+            using (var strem = new FileStream("out.bin", FileMode.Open, FileAccess.Read))
+            {
+                result = (Rectangle)formatter.Deserialize(strem);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
