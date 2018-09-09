e6a856b35945cce389ef5f5611c4cee6a3b89dec
xaste <xaste@mail.ru>
2018-09-09 12:44:06 +0700

    Graph

---
+++ CourseTasks/GraphExercise/GraphExercise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExercise
+{
+    class GraphExercise
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new double[,]
+            {
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 0, 1, 1, 1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 0, 0, 0},{0,0,0,0,0,0,0,0,1,1},{0,0,0,0,0,0,0,1,0,0}, {0,0,0,0,0,0,0,1,0,0}
+            };
+
+            var graph1 = new MyGraph(array1);
+
+            graph1.GoThroughWide();
+
+        }
+    }
+}
---
+++ CourseTasks/GraphExercise/MyGraph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExercise
+{
+    class MyGraph
+    {
+        private readonly double[][] graph;
+
+        public int Count { get; set; }
+
+        public MyGraph(double[,] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Число столбцов или колонок не может быть 0");
+            }
+
+            graph = new double[array.GetLength(0)][];
+
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                graph[i] = new double[array.GetLength(1)];
+
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    graph[i][j] = array[i, j];
+                }
+            }
+
+            Count = array.GetLength(0);
+        }
+
+        public void GoThroughWide()
+        {
+            var visited = new bool[Count];
+
+            var queue = new Queue<int>();
+
+            for (var i = 0; i < visited.Length; i++)
+            {
+                if (visited[i] != false)
+                {
+                    continue;
+                }
+
+                queue.Enqueue(i);
+
+                while (queue.Count > 0)
+                {
+                    var row = queue.Dequeue();
+
+                    if (visited[row])
+                    {
+                        continue;
+                    }
+
+                    //work
+
+                    visited[row] = true;
+
+                    for (var j = 0; j < graph[row].Length; j++)
+                    {
+                        if (graph[row][j] > 0)
+                        {
+                            queue.Enqueue(j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
