5ebd142c7f2a2f8b6ac64fb5f64cbb7e208865a4
xaste <xaste@mail.ru>
2018-10-15 14:14:10 +0700

    FileConfig

--- ServerWeb/ConfigFile/ConfigFile.cs
+++ ServerWeb/ConfigFile/ConfigFile.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConfigFile
 {
-    class Program
+    class ConfigFile
     {
         static void Main(string[] args)
         {
+            var siteUrl = ConfigurationManager.AppSettings["SiteUrl"];
+
+            Console.WriteLine(siteUrl);
         }
     }
 }
