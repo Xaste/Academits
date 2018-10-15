d97aafcd66114fe2fbf690f67a343e92cf008d8c
xaste <xaste@mail.ru>
2018-10-15 15:34:30 +0700

    Excel

---
+++ ServerWeb/Excel/ExcelExercise.cs
@@ -0,0 +1,43 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel
+{
+    class ExcelExercise
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+            persons.Add(new Person() { Age = 20, Name = "Alex", Surname = "Morgan", PhoneNumber = "123456" });
+            persons.Add(new Person() { Age = 22, Name = "Ann", Surname = "Smith", PhoneNumber = "654321" });
+
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Лист1");
+
+            worksheet.Cell("A" + 1).Value = "Имя";
+            worksheet.Cell("B" + 1).Value = "Фамилия";
+            worksheet.Cell("C" + 1).Value = "Возраст";
+            worksheet.Cell("D" + 1).Value = "Тел. номер";
+
+            for (int i = 0; i < persons.Count; i++)
+            {
+                worksheet.Cell(i + 2, 1).Value = persons[i].Name;
+                worksheet.Cell(i + 2, 2).Value = persons[i].Surname;
+                worksheet.Cell(i + 2, 3).Value = persons[i].Age;
+                worksheet.Cell(i + 2, 4).Value = persons[i].PhoneNumber;
+            }
+
+            worksheet.Columns().AdjustToContents();
+
+            using (FileStream stream = new FileStream("out.xls", FileMode.Create, FileAccess.Write))
+            {
+                workbook.SaveAs(stream);
+            }
+        }
+    }
+}
---
+++ ServerWeb/Excel/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel
+{
+    class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string  Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
