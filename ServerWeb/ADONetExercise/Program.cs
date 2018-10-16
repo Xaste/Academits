246c0810b48a7ee71abf0131c927de78dd6c963d
xaste <xaste@mail.ru>
2018-10-16 17:13:29 +0700

    ADO.NET

---
+++ ServerWeb/ADONetExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONetExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            var dbShop = new DBShop(connectionString);
+
+            Console.WriteLine($"Всего товаров: {dbShop.GetProductCount()}");
+
+            //dbShop.CreateProduct("intel Core i10", 1);
+
+            //dbShop.CreateCategory("Блоки питания");
+
+            dbShop.UpdateProduct("Intel Core i77", 1, "Name = Intel Core i7");
+        }
+    }
+}
