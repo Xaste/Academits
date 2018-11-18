520e902fd241e4286d12f7b17dad3e040271a822
xaste <xaste@mail.ru>
2018-11-18 17:23:00 +0700

    Patterns_Strategy

---
+++ Patterns/Strategy_Duck/Duck.cs
@@ -0,0 +1,48 @@
+﻿using Duck.FlyBehavior;
+using Duck.QuackBehavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Duck
+{
+    public abstract class Duck
+    {
+        protected IFlyBehavior _flyBehavior;
+        protected IQuackBehavior _quackBehavior;
+
+        public Duck()
+        {
+        }
+
+        public abstract void Display();
+
+        public void PerformFly()
+        {
+            _flyBehavior.Fly();
+        }
+
+        public void PerformQuack()
+        {
+            _quackBehavior.Quack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+
+        public void SetFlyBehavior(IFlyBehavior fb)
+        {
+            _flyBehavior = fb;
+        }
+
+        public void SetQuackBehavior(IQuackBehavior qb)
+        {
+            _quackBehavior = qb;
+        }
+    }
+}
